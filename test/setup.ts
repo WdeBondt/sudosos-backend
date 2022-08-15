6132f73d0cb37c3556ddffcc5fef7d3b85f614d5
justsamuel <samuel.oosterholt20001004@gmail.com>
2022-08-15 13:21:10 +0200

    New script to use env variables from runner
    Signed-off-by: justsamuel <samuel.oosterholt20001004@gmail.com>

--- test/setup.ts
+++ test/setup.ts
@@ -34,9 +34,6 @@ use(sinonChai);
 
 process.env.NODE_ENV = 'test';
 process.env.HTTP_PORT = '3001';
-process.env.TYPEORM_CONNECTION = 'sqlite';
-process.env.TYPEORM_DATABASE = ':memory:';
-process.env.TYPEORM_SYNCHRONIZE = 'true';
 
 dinero.defaultCurrency = 'EUR';
 dinero.defaultPrecision = 2;
